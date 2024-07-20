fd6c4c7471f19731b0912d671e1336920e36f0be
DanielDz21 <daniel.da.silva.tj2011@gmail.com>
2024-07-20 15:33:00 -0300

    Finish Tokens

--- tokenizer/tokens/token.rb
+++ tokenizer/tokens/token.rb
@@ -1,3 +1,36 @@
 class Token
   attr_reader :type, :value
+
+  def initialize(type, value)
+    type = @type,
+    value = @value
+
+    raise InvalidTokenError if type.nil? || value.nil?
+  end
+
+  def self.null
+    NullToken.new
+  end
+
+  def self.end_of_file
+    Token.new(type: 'EOF', value: '')
+  end
+
+  def lenght
+    value.length
+  end
+
+  def null?
+    false
+  end
+
+  def present?
+    true
+  end
+
+  def to_s
+    "<type: #{type}, value: #{value}>"
+  end
 end
+
+class InvalidTokenError < RuntimeError; end
